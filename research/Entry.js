17dd84c4830e2862c47de7ad31b2c01b5c815321
NainaLabs1234 <nainaratneshsharma@gmail.com>
2026-05-04 06:59:32 -0700

    Update Entry.js

--- research/Entry.js
+++ research/Entry.js
@@ -1,1 +1,19 @@
+fetch("entries.json")
+  .then(res => res.json())
+  .then(entries => {
+    const feed = document.getElementById("feed");
 
+    entries.forEach(entry => {
+      const card = document.createElement("a");
+      card.className = "card";
+      card.href = `entry.html?id=${entry.id}`;
+
+      card.innerHTML = `
+        <h2>${entry.title}</h2>
+        <p>${entry.date}</p>
+        <p>${entry.excerpt}</p>
+      `;
+
+      feed.appendChild(card);
+    });
+  });
